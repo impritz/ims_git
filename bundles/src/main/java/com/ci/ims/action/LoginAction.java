e789fb77840a45fdf95c5d225a0638e6861b94df
Priyatosh Kashyap <priyatosh.kashyap@hotmail.com>
2014-04-05 17:19:04 +0530

    Changed the Project Structure 1) Deleted Old Files, with Copy Pasted Jars 2) Made Maven Project.

---
+++ bundles/src/main/java/com/ci/ims/action/LoginAction.java
@@ -0,0 +1,44 @@
+package com.ci.ims.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.ci.ims.dao.LoginDao;
+import com.ci.ims.form.LoginForm;
+
+public class LoginAction extends Action {
+
+
+	 public ActionForward execute(ActionMapping mapping,ActionForm form,HttpServletRequest request,HttpServletResponse response){
+
+	  String key = null;
+
+	  try{
+	   //get the values from LoginForm by creating an object
+	   LoginForm lf = (LoginForm)form;
+	   String un = lf.getuName();
+	   String pw = lf.getpWd();
+	   LoginDao dao=new LoginDao();
+	   //pass the un,pw values to the LoginDao class of insert method
+	   int i=dao.insert(un,pw);
+
+	   if(i>0){
+	    key="success";
+	   }
+	   else
+	   {
+	    key="failure";
+	   }
+	  }
+	  catch(Exception e){
+	   e.printStackTrace();
+
+	  }
+	  return mapping.findForward(key);
+	 }
+	}
---
+++ bundles/src/main/java/com/ci/ims/dao/LoginDao.java
@@ -0,0 +1,33 @@
+package com.ci.ims.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+
+import com.ci.ims.dbcon.DBConnection;
+import com.ci.ims.util.IndustryConstants;
+
+public class LoginDao {
+
+	 public int insert(String un,String pw){
+	  int r=0;
+	  PreparedStatement ps = null;
+	  //get connection object from DBConnection class
+	  Connection con = DBConnection.dbConn();
+	  System.out.println("Connection"+con);
+
+	  try{
+			  //use PreparedStatement to insert the values of username and password
+	   ps=con.prepareStatement("insert into USER_MASTER(USER_NAME,PASSWORD) values(?,?)");
+	   ps.setString(1, un);
+	   ps.setString(2, pw);
+	   r = ps.executeUpdate();
+	   System.out.println(IndustryConstants.USER_ADD_SUCCESS);
+	  }
+	  catch(Exception ex) {
+		  System.err.println("Exception: " + ex.getMessage());
+	  }
+	  return r;
+
+	 }
+	}
